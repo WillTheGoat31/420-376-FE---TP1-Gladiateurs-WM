28d28e4b70fc35192e85b042820e7033d4782681
WillTheGoat31 <williammartel31@icloud.com>
2024-09-10 11:05:54 -0400

    Commence calcul force d'attaque

--- JeuGladiateurs/src/combat/CompteurDeTour.java
+++ JeuGladiateurs/src/combat/CompteurDeTour.java
@@ -9,33 +9,41 @@ public class CompteurDeTour {
     // TODO : Mettre vos attributs ici
     // </editor-fold>
     
+    private int cptrTour;
+    
     // **************************************************************************
     // **************************************************************************
     // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Constructeurs et initialisation">
     public CompteurDeTour() {
-        // TODO : Constructeur SANS paramètres qui initialise un compteur de tour (attribut) à 1
+        cptrTour = 1;
     }
-    // </editor-fold>
 
+    
     // **************************************************************************
     // **************************************************************************
     // **************************************************************************
-    // <editor-fold defaultstate="collapsed" desc="Getters et setters">
-    // TODO : Le getter
-    // TODO : Le setter
-    // </editor-fold>
+    public int getCptrTour() {
+        return cptrTour;
+    }
+    public void setCptrTour(int cptrTour) {
+        this.cptrTour = cptrTour;
+    }
+    
+    
 
     // **************************************************************************
     // **************************************************************************
     // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Mécanique de jeu">
     public void augmenteTour() {
-        // TODO : Incrémenter le compteur de tour
+        cptrTour += 1;
     }
 
     public void afficheTour() {
-        // TODO : Afficher le tour actuel, tel que montré dans l'énoncé
+        System.out.println("---------------");
+        System.out.println("TOUR ACTUEL : " + cptrTour);
+        System.out.println("---------------");
     }
-    // </editor-fold>
+    
 }
